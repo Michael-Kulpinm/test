069c2b675797a141577a4a90a25ea2288feb5588
unknown <kulpinm@mail.ru>
2020-07-14 16:08:12 +0300

    First commit

---
+++ function/functionOne.js
@@ -0,0 +1,15 @@
+function hello(){
+    alert('Hello world!')
+}
+hello();
+
+function greetGuest(){
+    let str
+    if(str!=undefined){
+        alert('Hello String')
+    }
+    else{
+        alert('Hello Guest')
+    }
+}
+greetGuest();
